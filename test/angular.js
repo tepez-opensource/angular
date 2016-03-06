37e37d9bc1a77081ed260174b01e9a5f6b167b76
Tom Yam <tomyam1@gmail.com>
2016-03-06 18:47:15 +0200

    tidy up specs in es6

--- test/angular.js
+++ test/angular.js
@@ -1,17 +1,16 @@
-var expect = require('chai').expect,
+const expect = require('chai').expect,
   path = require('path');
 
-function scriptPath(script) {
-  return path.join(__dirname, '..', 'bower_components', script);
-}
 
+describe('angular', () => {
 
-describe('angular', function () {
+  let spec;
 
-  var angular, window;
+  function scriptPath(script) {
+    return path.join(__dirname, '..', 'bower_components', script);
+  }
 
-  function load() {
-
+  function loadModules() {
     [
       '..',
       '../window',
@@ -20,15 +19,18 @@ describe('angular', function () {
       scriptPath('angular-mocks/angular-mocks.js'),
       scriptPath('angular-sanitize/angular-sanitize.js'),
       scriptPath('jasmine-jquery/lib/jasmine-jquery.js')
-    ].forEach(function (script) {
-//        console.log(require.resolve(script));
-        delete require.cache[require.resolve(script)];
+    ].forEach((script) => {
+      delete require.cache[require.resolve(script)];
     });
-    angular = require('..');
-    window = require('../window');
+    spec.angular = require('..');
+    spec.window = require('../window');
   }
 
   beforeEach(function () {
+    spec = this;
+  });
+
+  beforeEach(() => {
     process.env.TP_JQUERY_PATH = scriptPath('jquery/dist/jquery.js');
     process.env.TP_ANGULAR_PATH = scriptPath('angular/angular.js');
     process.env.TP_ANGULAR_MOCKS_PATH = scriptPath('angular-mocks/angular-mocks.js');
@@ -36,72 +38,64 @@ describe('angular', function () {
     process.env.TP_JASMINE_JQUERY_PATH = scriptPath('jasmine-jquery/lib/jasmine-jquery.js');
   });
 
-  describe('when global.jasmine is not defined', function () {
+  describe('when global.jasmine is not defined', () => {
+    beforeEach(loadModules);
 
-    beforeEach(load);
-
-    it('angular should work', function () {
-      angular.injector(['ng']).invoke(function ($rootScope, $compile) {
-        var el = angular.element('<div>{{ 2 + 2 }}</div>');
+    it('angular should work', () => {
+      spec.angular.injector(['ng']).invoke(($rootScope, $compile) => {
+        var el = spec.angular.element('<div>{{ 2 + 2 }}</div>');
         el = $compile(el)($rootScope);
         $rootScope.$digest();
         expect('' + el.html()).to.equal('4')
       });
     });
 
-    it('angular should be able to sanitize html', function () {
-      angular.injector(['ng', 'ngSanitize']).invoke(function ($sce) {
+    it('angular should be able to sanitize html', () => {
+      angular.injector(['ng', 'ngSanitize']).invoke(($sce) => {
         expect($sce.getTrustedHtml('xxx<script>yyy</script>zzz')).to.equal('xxxzzz');
       });
     });
 
-    it('window.angular should be angular', function () {
-      expect(window.angular.version).to.be.an('object');
+    it('window.angular should be angular', () => {
+      expect(spec.window.angular.version).to.be.an('object');
     });
 
-    it('window.$ and window.jQuery should be jQuery', function () {
-      expect(window.$).to.equal(window.jQuery);
-      expect(window.$.fn.jquery).to.be.a('string');
+    it('window.$ and window.jQuery should be jQuery', () => {
+      expect(spec.window.$).to.equal(spec.window.jQuery);
+      expect(spec.window.$.fn.jquery).to.be.a('string');
     });
-
   });
 
-
-  describe('when global.jasmine is defined', function () {
-
-    var _beforeEach, _afterEach;
-
-    beforeEach(function () {
+  describe('when global.jasmine is defined', () => {
+    beforeEach(() => {
       global.jasmine = {};
       // we must change the global beforeEach and afterEach because angular-mocks
       // calls them and this interferes we our mocha tests
-      _beforeEach = beforeEach;
-      _afterEach = afterEach;
+      spec._beforeEach = beforeEach;
+      spec._afterEach = afterEach;
       global.beforeEach = function() {};
       global.afterEach = function() {};
-      load();
+      loadModules();
     });
 
-    afterEach(function () {
-      global.beforeEach = _beforeEach;
-      global.afterEach = _afterEach;
+    afterEach(() => {
+      global.beforeEach = spec._beforeEach;
+      global.afterEach = spec._afterEach;
       delete global.jasmine;
     });
 
-    it('angular be able to mock module', function () {
-      expect(angular.mock.module).to.be.a('Function');
+    it('angular be able to mock module', () => {
+      expect(spec.angular.mock.module).to.be.a('Function');
     });
 
-    it('window should have jasmine, beforeEach and afterEach', function () {
-      expect(window.jasmine).to.equal(global.jasmine);
-      expect(window.beforeEach).to.equal(global.beforeEach);
-      expect(window.afterEach).to.equal(global.afterEach);
+    it('window should have jasmine, beforeEach and afterEach', () => {
+      expect(spec.window.jasmine).to.equal(global.jasmine);
+      expect(spec.window.beforeEach).to.equal(global.beforeEach);
+      expect(spec.window.afterEach).to.equal(global.afterEach);
     });
 
-    it('jasmine-jquery should have been loaded', function () {
-      expect(window.jasmine.jQuery).to.be.an('Function');
+    it('jasmine-jquery should have been loaded', () => {
+      expect(spec.window.jasmine.jQuery).to.be.an('Function');
     });
-
   });
-
 });
